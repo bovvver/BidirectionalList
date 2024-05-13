482076b2591a52d4059ea4ebcb87f62764e049a3
Jakub Wroński <wronski2002@wp.pl>
2024-05-13 12:20:17 +0200

    Added binary search tree (BST) implementation

---
+++ BinaryTree/TreeNode.cs
@@ -0,0 +1,19 @@
+﻿namespace BinaryTree
+{
+    internal class TreeNode
+    {
+        public float Value {  get; set; }
+        public TreeNode? Left { get; set;}
+        public TreeNode? Right { get; set;}
+
+        public TreeNode(float value, TreeNode? left, TreeNode? right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        public TreeNode(float value): this (value, null, null)
+        {}
+    }
+}
