27312f589f42378ba92e7da255bf72d337e1abd7
Jakub Wroński <wronski2002@wp.pl>
2024-05-27 12:45:07 +0200

    Added basic graph implementation

---
+++ Graph/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Graph;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var graph = new Graph<int>();
+
+        graph.AddNode(1);
+        graph.AddNode(1, 2);
+        graph.AddNode(1, 3);
+        graph.AddNode(2, 3);
+
+        graph.AddNode(2, 4);
+        graph.AddNode(2, 5);
+
+        graph.AddNode(3, 6);
+        graph.AddNode(3, 7);
+        graph.AddNode(7, 8);
+        graph.AddNode(6, 8);
+
+        graph.DFS(1, 4);
+        graph.BFS(1, 4);
+    }
+}
